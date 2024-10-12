8a72f7dbb03162e4e429d371ea1f9c18c1f95886
Juanco <juanffo874@gmail.com>
2024-10-11 20:03:00 -0600

    Crea la pagina de no encotrado y la añade a las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './views/Home'
+import NotFound from './NotFound'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home/>}/>
+        <Route path='*' element={<NotFound/>}/>
       </Routes>
     </>
   )
