a09008d563fe471aa91751455902f981974b0409
Juanco <juanffo874@gmail.com>
2024-10-12 21:58:51 -0600

    Crea la vista del registro de usuarios y la añade a las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './views/Home'
 import NotFound from './NotFound'
 import Login from './views/Auth/Login'
+import Signin from './views/Auth/Signin'
 import ForgotPassword from './views/Auth/ForgotPassword'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path='*' element={<NotFound/>}/>
         <Route path='/auth/singin' element={<Login/>}/>
         <Route path='/auth/forgot-password' element={<ForgotPassword/>}/>
+        <Route path='/auth/signup' element={<Signin/>}/>
       </Routes>
     </>
   )
