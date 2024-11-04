90bec428201a1a83c731d29c41b335aebd6a219b
Juanco <juanffo874@gmail.com>
2024-11-03 19:24:08 -0600

    Crea estilos personalizados

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ export default {
     extend: {
       blur: {
         xs: '0.5px'
+      },
+      screens: {
+        'custom-sm': '500px', // Cambia 500px al valor deseado
+        'custom-md': '750px',
+        'custom-lg': '1000px',
       }
     },
   },
