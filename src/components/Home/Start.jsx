5c85b775736841c1f3ab3153ae439e25da8c2429
Juanco <juanffo874@gmail.com>
2024-11-09 23:36:16 -0600

    Actualizado el boton

--- src/components/Home/Start.jsx
+++ src/components/Home/Start.jsx
@@ -16,7 +16,7 @@ const Start = () => {
           </p>
           <Link
             to="/auth/signup"
-            className="bg-gray-800 mt-4 text-white py-2 px-8 rounded-lg hover:bg-gray-700 flex items-center font-semibold"
+            className="bg-gray-800 mt-4 text-white py-2 px-8 rounded-md hover:bg-gray-700 flex items-center font-semibold"
           >
             Comieza tu recorrido por Taski
             <span className="material-symbols-rounded ml-2">
