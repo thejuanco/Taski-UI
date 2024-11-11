961ca6334a3d49c4a2d0bd540cd02effb03cce30
Juanco <juanffo874@gmail.com>
2024-11-10 19:28:38 -0600

    Crea la instancia de axios y crea la primera peticion hacia el backend, e instala axios

---
+++ src/api/user.api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const authAPI = axios.create({
+    baseURL: "https://taski-backend-q5v2.onrender.com/",
+    // headers: {
+    //     "Authorization": "Bearer your_api_token"
+    // }
+})
+
+//Registrar los usuarios
+export const registerUser = async (data) => {
+    return await authAPI.post('/auth/createUser', data)
+}
