a39160c2ebea1282cb31ec059126b77d57a5c020
Juanco <juanffo874@gmail.com>
2025-01-28 21:17:28 -0600

    Modifica el boton para redirigir al demo y añade un boton para añadir un boton y un titulo

--- src/views/layouts/Demo/DemoTasks.jsx
+++ src/views/layouts/Demo/DemoTasks.jsx
@@ -5,8 +5,15 @@ const DemoTasks = () => {
   return (
     <>
         <DemoSidebar />
-        <div>
-            <h1 className='font-bold text-3xl mb-4'>Tareas</h1>
+        <div className='p-6 sm:ml-64'>
+            <div className='flex justify-between'>
+                <h1 className='font-bold text-3xl mb-4'>Tareas</h1>
+                <div className='mr-4'>
+                    <button className='bg-gray-900 text-gray-100 rounded-full px-6 py-2 hover:bg-gray-800'>
+                        Añadir tarea
+                    </button>
+                </div>
+            </div>
         </div>
     </>
   )
